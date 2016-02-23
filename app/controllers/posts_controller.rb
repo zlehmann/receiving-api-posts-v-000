14c7110d9b80b2cd1e9fd494e3177e64e62cd5c2
Scott C. Reynolds <scott@scottcreynolds.com>
2016-02-23 03:14:08 -0500

    first draft

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-    render json: @post, status: 201
+    redirect_to post_path(@post)
   end
 
   def edit
