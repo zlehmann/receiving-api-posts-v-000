fb390c026fc4d3495e41a3c6b79f1aae92557538
maxwellbenton <maxwellbenton@gmail.com>
2019-02-13 17:08:04 -0500

    rails app:update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  resources :authors, only: [:show]
-  resources :posts, except: [:delete]
-  get 'posts/:id/post_data', to: 'posts#post_data'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
