59bd36bb8ed03cdcf18ded55cc79d0272bc5c446
Zachary Lehmann <zlehmann@gmail.com>
2019-04-19 17:12:06 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-    redirect_to post_path(@post)
+    render json: @post, status: 201
   end
 
   def edit
